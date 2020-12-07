86ea9a9a1c2641fa2b21b802544412e3c6eacdcc
Jonathan Chang <me@jonathanchang.org>
2020-12-07 18:17:53 +1100

    kolmafia (new formula)

---
+++ Formula/kolmafia.rb
@@ -0,0 +1,20 @@
+class Kolmafia < Formula
+  desc "Interface for online game"
+  homepage "https://kolmafia.us"
+  url "https://svn.code.sf.net/p/kolmafia/code/", revision: 20548
+  version "20548"
+  head "https://svn.code.sf.net/p/kolmafia/code/"
+
+  depends_on "ant" => :build
+  depends_on "openjdk"
+
+  def install
+    system "ant", "daily", "-Drevision=#{version}"
+    libexec.install Dir["dist/*.jar"]
+    bin.write_jar_script libexec/"KoLmafia-#{version}.jar", "kolmafia"
+  end
+
+  test do
+    assert_match(version.to_s, shell_output("#{bin}/kolmafia --version"))
+  end
+end
