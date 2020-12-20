e6b5481c63df2c53f2c6fbccf8b893c0a9c7bf35
Jonathan Chang <me@jonathanchang.org>
2020-12-20 00:08:58 +0000

    update

--- Formula/kolmafia.rb
+++ Formula/kolmafia.rb
@@ -2,7 +2,7 @@ class Kolmafia < Formula
   desc "Interface for online game"
   homepage "https://kolmafia.us"
   url "https://svn.code.sf.net/p/kolmafia/code/", revision: version
-  version "20577"
+  version "20578"
   license "BSD-3-Clause"
   head "https://svn.code.sf.net/p/kolmafia/code/"
 
