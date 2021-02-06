32c4a96d47a1f63abc43a6644346a557c0eae279
Jonathan Chang <me@jonathanchang.org>
2021-02-06 00:23:05 +0000

    kolmafia: update 20630 bottle.

--- Formula/kolmafia.rb
+++ Formula/kolmafia.rb
@@ -13,9 +13,9 @@ class Kolmafia < Formula
   end
 
   bottle do
-    root_url "https://github.com/jonchang/homebrew-tap/releases/download/kolmafia-20629"
-    sha256 cellar: :any_skip_relocation, catalina:     "cb3d37a46d03a863fbabcc3cd4d9cad2786245863744fb0837af15be571ad7c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5706560285925589df77e36f8906ed159190b94629ea8ef7b91f3e97e0070447"
+    root_url "https://github.com/jonchang/homebrew-tap/releases/download/kolmafia-20630"
+    sha256 cellar: :any_skip_relocation, catalina:     "83515556b40ccd081093d586d140be293a4f8369e30d04db4827eadd75c98df0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d94030e539c156b7b2989717598d8cc84f6e2e47369791015fff5f0136b96043"
   end
 
   depends_on "ant" => :build
