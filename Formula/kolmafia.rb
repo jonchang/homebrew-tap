b7af276778cc26f70cb5297aacc18671b870e183
Jonathan Chang <me@jonathanchang.org>
2021-08-10 02:39:17 +0000

    kolmafia 20812
    update
    
    Closes #118.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/kolmafia.rb
+++ Formula/kolmafia.rb
@@ -2,7 +2,7 @@ class Kolmafia < Formula
   desc "Interface for online game"
   homepage "https://kolmafia.us"
   url "https://svn.code.sf.net/p/kolmafia/code/", revision: version
-  version "20811"
+  version "20812"
   license "BSD-3-Clause"
   head "https://svn.code.sf.net/p/kolmafia/code/"
 
